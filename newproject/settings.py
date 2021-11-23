9284fc761cb26cb4bb327f3f361556a5c50b8c62
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2021-11-23 11:00:09 +0530

    version3

--- newproject/settings.py
+++ newproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0)!2n%&gc5udm0bhr)6i_vt6u85mh_8j^2)7*l%l7(t#3le11+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
